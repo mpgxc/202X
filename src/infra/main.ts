d111e29700abee5c237b96b35c4db6dcc8ea1849
mpgxc <mpgx5.c@gmail.com>
2024-04-18 01:28:21 -0300

    style: setup biome

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,7 +1,21 @@
+import http from "node:http";
+
+const port = process.env.PORT || 3000;
+
 console.log("ENV:", process.env.NODE_ENV);
 
-async function bootstrap(): Promise<any> {
-  return Promise.resolve(console.info("Hello World"));
+async function bootstrap(): Promise<unknown> {
+	return Promise.resolve(console.info("Hello World"));
 }
 
 bootstrap();
+
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader("Content-Type", "text/plain");
+	res.end("Hello, World  !  ");
+});
+
+server.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`);
+});
